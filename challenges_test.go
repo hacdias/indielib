32d44cd9def9dbc075d101dff025a0ab126f66f5
Henrique Dias <hacdias@gmail.com>
2023-11-02 10:26:10 +0100

    refactor: use testify on existing tests

--- challenges_test.go
+++ challenges_test.go
@@ -6,24 +6,21 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestPlainChallenge(t *testing.T) {
 	// Generate a code_verifier within the allowed minimum length of 43 characters and maximum
 	// length of 128 characters, per https://datatracker.ietf.org/doc/html/rfc7636#section-4.1.
-	if !ValidateCodeChallenge("plain", strings.Repeat("a", 43), strings.Repeat("a", 43)) {
-		t.Error("plain challenge must be valid")
-	}
+	require.True(t, ValidateCodeChallenge("plain", strings.Repeat("a", 43), strings.Repeat("a", 43)))
 }
 
 func TestS256Challenge(t *testing.T) {
 	cv, err := newVerifier()
-	if err != nil {
-		t.Fatal("newVerifier:", err)
-	}
-	if len(cv) < 43 || len(cv) > 128 {
-		t.Fatal(ErrWrongCodeVerifierLength)
-	}
+	require.NoError(t, err)
+	require.False(t, len(cv) < 43 || len(cv) > 128)
+
 	cc := s256Challenge(cv)
 	if got, want := len(cc), s256ChallengeLength; got != want {
 		err := fmt.Errorf("S256 challenge length is %d, want %d", got, want)
@@ -32,9 +29,7 @@ func TestS256Challenge(t *testing.T) {
 		}
 		t.Fatal(err)
 	}
-	if !ValidateCodeChallenge("S256", cc, cv) {
-		t.Error("S256 challenge must be valid")
-	}
+	require.True(t, ValidateCodeChallenge("S256", cc, cv))
 }
 
 // s256ChallengeLength is the expected length of the code challenge
@@ -42,7 +37,5 @@ func TestS256Challenge(t *testing.T) {
 const s256ChallengeLength = 43
 
 func TestS256ChallengeLength(t *testing.T) {
-	if s256ChallengeLength != base64.RawURLEncoding.EncodedLen(sha256.Size) {
-		t.Errorf("internal error: s256ChallengeLength != base64.RawURLEncoding.EncodedLen(sha256.Size)")
-	}
+	require.Equal(t, s256ChallengeLength, base64.RawURLEncoding.EncodedLen(sha256.Size))
 }
